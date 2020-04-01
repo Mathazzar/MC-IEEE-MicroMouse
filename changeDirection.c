f674c24050a5fb1375c0f20bc72268d4265fdccf
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-04-01 18:56:32 -0400

    Update changeDirection.c
    Add documentation and description of use cases.

--- changeDirection.c
+++ changeDirection.c
@@ -2,11 +2,22 @@
 Written by Mathazzar
 Last modified: 04/1/20
 Purpose: update direction the micromouse is facing in relation to its original position.
-Status: UNFINISHED, NOT TESTED
+Status: UNFINISHED, PARTIALLY TESTED
 */
 
 //#include<stdio.h>
 
+/*int changeDirection(short int direction, short int type)
+INPUTS: direction, type
+	direction: current orientation of micromouse in relation to original position.
+	type: change in orientation relative to previous orientation.
+	Both are read as 0=UP, 1=DOWN, 2=RIGHT, 3=LEFT, and all other cases are ignored.
+RETURNS: dire
+	dire: a new variable for the new direction, which is returned.
+NOTES: implemented to be called as:
+	direction = changeDirection(direction, type);
+	to set direction to new orientation.
+*/
 int changeDirection(short int direction, short int type) //Records changes in direction
 {
 	short int dire;
@@ -19,7 +30,7 @@ int changeDirection(short int direction, short int type) //Records changes in di
 		{
 		case 0:
 			dire = 1;
-				break;
+			break;
 		case 1:
 			dire = 0;
 			break;
