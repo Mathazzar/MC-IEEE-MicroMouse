2767cdc08c99803eced5fea2d6625795480f0f1a
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-04-17 19:45:14 -0400

    Update updateDir.c

--- updateDir.c
+++ updateDir.c
@@ -1,25 +1,42 @@
+
+#include <stdbool.h>	
+#include <stdio.h>
+
+void simLog(char* text); //modified from main.c in mms example (https://github.com/mackorone/mms-c)
+
 short int updateDir(short int direction, short int relativeChange)
 {
 	short int dire = direction;
+	fprintf(stderr, "DIRECTION: %d\n", dire);
+	fflush(stderr);
 	switch (relativeChange)
 	{
 	case 0: //straight
+		simLog("Turning Straight?");
 		break;
 	case 1: //turn right
+		simLog("Turning Right");
 		dire++;
-		if (dire >= 4)
+		if (dire > 3)
 			dire -= 4;
+		fprintf(stderr, "DIRECTION: %d\n", dire);
+		fflush(stderr);
 		break;
 	case 2: //about face
-		dire++;
-		dire++;
-		if (dire >= 4)
+		simLog("About Face");
+		dire += 2;
+		if (dire > 3)
 			dire -= 4;
+		fprintf(stderr, "DIRECTION: %d\n", dire);
+		fflush(stderr);
 		break;
 	case 3: //turn left
+		simLog("Turning Left");
 		dire--;
-		if (dire <= -1)
+		if (dire < 0)
 			dire += 4;
+		fprintf(stderr, "DIRECTION: %d\n", dire);
+		fflush(stderr);
 		break;
 	}
 	return dire;
